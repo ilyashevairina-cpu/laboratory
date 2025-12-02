6edde0d24494bb8f3066e7339aeb18e475bfee1d
ilyashevairina-cpu <ilyasheva.irina@gmail.com>
2025-12-02 15:22:32 +0300

    добавил все файлы сайта

---
+++ app-files/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Офис",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7388729156202594,
+        "pitch": 0.21631132540198017,
+        "fov": 1.3699474477677758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6640875254432004,
+          "pitch": 0.7421459806728201,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6768762122124574,
+        "pitch": 0.3331699785447988,
+        "fov": 1.3699474477677758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5147643232658865,
+          "pitch": 0.6590326190976228,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.7086969260698615,
+          "pitch": 0.805374992319237,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Щековая дробилка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5360860963993268,
+        "pitch": 0.2935537320220618,
+        "fov": 1.3699474477677758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6527806334049844,
+          "pitch": 0.5646170102607808,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.5931544594791038,
+          "pitch": 0.7308565201351875,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Валковая дробилка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6324339298730415,
+        "pitch": 0.2371456995328245,
+        "fov": 1.3699474477677758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6357561851751612,
+          "pitch": 0.6124527023193487,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.6774847726202538,
+          "pitch": 0.5330051337059345,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Стеллажи",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.62431433101038,
+        "pitch": 0.1378573936987948,
+        "fov": 1.3699474477677758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5482776147344541,
+          "pitch": 0.7274742001801489,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Лаборатория",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
